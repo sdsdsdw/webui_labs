cafff55ccda327dc0f29b77f2031cdf33bcb93b8
sdsdsdw <148587087+sdsdsdw@users.noreply.github.com>
2025-09-22 12:51:23 +0000

    task 1

--- lab3/main.js
+++ lab3/main.js
@@ -1,64 +1,16 @@
-// example 1
-var today= new Date();
-var god = today.getYear();
+function showDate() {
+	var days = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота"];
+	var months = ["січня", "лютого", "березня", "квітня", "травня", "червня",
+				"липня", "серпня", "вересня", "жовтня", "листопада", "грудня"];
 
-alert(god);
+	var now = new Date();
 
+	var weekday = days[now.getDay()];
+	var day = now.getDate();
+	var month = months[now.getMonth()];
+	var year = now.getFullYear();
 
-// example 2
-var dr = new Date(1958, 4, 21);
-dn = dr.getDay()
-alert(dn)
+	var text = "Сегодня " + day + " " + month + " " + year + " " + weekday;
 
-
-// example 3
-var dr = new Date(1958, 4, 21);
-var dn = dr.getDay();
-var dayn = new Array(
-	'неділя',
-	'понеділок',
-	'вівторок',
-	'середа',
-	'четвер',
-	"п'ятниця",
-	'субота'
-);
-alert(dayn[dn]);
-
-
-// example 4
-function gd()
-{
-	var today= new Date();
-	var god=today.getYear();
-
-	p1=document.getElementById('f1');
-	p1.value=god;
+	document.getElementById("today").textContent = text;
 }
-
-
-// example 5
-today = new Date();
-
-var y = today.getYear(); 
-var yearBegin = new Date(y,0,1); 
-var d = today.getTime() - yearBegin.getTime(); 
-
-d = d / 86400000; 
-d = d + 1; 
-d = Math.floor(d); 
-
-document.write("<h2>Сьогодні " + d + " день з початку року</h2>");
-
-
-// example 6
-
-function sqr()
-{
-	p = document.getElementById('t');
-	r = p.value;
-	var s = Math.PI * r * r;
-	alert(s)
-}
-
-// -------- //
