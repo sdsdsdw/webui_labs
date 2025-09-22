ea9a0c60b45d7ad08104b748bb34950b36f0289f
sdsdsdw <148587087+sdsdsdw@users.noreply.github.com>
2025-09-22 23:33:01 +0000

    task 3

--- lab3/main.js
+++ lab3/main.js
@@ -1,14 +1,9 @@
-function outDays() {
-    let year = prompt("Введи рік народження:");
-    let month = prompt("Введи місяць народження (1-12):") - 1;
-    let day = prompt("Введи день народження:");
+function calcDistance() {
+    let x = document.getElementById("x").value;
+    let y = document.getElementById("y").value;
 
-    let birthday = new Date(year, month, day);
-    let now = new Date();
-
-    let diff = now - birthday;
-    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-
-    let todayElement = document.getElementById("today");
-    todayElement.textContent = "Ти прожив " + days + " днів.";
+    let distance = Math.sqrt(x * x + y * y);
+ 
+    document.getElementById("result").textContent =
+    	"Відстань до початку координат: " + distance.toFixed(2);
 }
