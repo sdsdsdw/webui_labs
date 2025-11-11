5a9b49e8bf9d0b3813d88dac2babda1ed1a9b9a9
sdsdsdw <148587087+sdsdsdw@users.noreply.github.com>
2025-11-11 12:45:03 +0000

    example 3

--- lab6/main.js
+++ lab6/main.js
@@ -1,11 +1,15 @@
-var mess = new Array("", 
-    "Вимкнення комп'ютера",
-    "Видалення файлів із системного диска",
-    "Форматування вінчестера"
-);
+let i = 0;
 
-function doEvent(num)
-{
-    cv=document.getElementById('info');
-    cv.value= mess[num];
+function slideShow() {
+  const ris = ['7.png', '8.png', '9.png', '6.png'];
+
+  if (i >= ris.length) {
+    i = 0;
+  }
+
+  const r = document.getElementById('slide');
+  r.src = ris[i];
+  i++;
+
+  setTimeout(slideShow, 1500);
 }
