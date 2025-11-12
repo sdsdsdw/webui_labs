b25ec9509291b6b35a74ad10985bd1ba26d5befb
sdsdsdw <148587087+sdsdsdw@users.noreply.github.com>
2025-11-12 14:18:38 +0000

    example 4

--- lab7/main.js
+++ lab7/main.js
@@ -26,23 +26,45 @@
 // }
 
 
-let x = 0;
+// let x = 0;
 
-const r1 = document.getElementById('im1');
-const r2 = document.getElementById('im2');
+// const r1 = document.getElementById('im1');
+// const r2 = document.getElementById('im2');
+
+// window.onload = function() {
+//     const x1 = r1.offsetWidth;
+//     const x2 = r2.offsetWidth;
+//     function f() {
+//         x += 10;
+//         r2.style.left = x + 'px';
+//         if (x < x1 - x2) {
+//             setTimeout(f, 700);
+//         }
+//     }
+//     f();
+// };
+
+function f() {
+    x = x + s;
+    y = y + s;
+
+    r2.style.left = x + 'px';
+    r2.style.top  = y + 'px';
+
+    if (x >= x1 - r2.offsetWidth || y >= y1 - r2.offsetHeight) s = -10;
+
+    if (x <= 0 || y <= 0) s = 10;
+
+    setTimeout("f()", 50);
+}
+
+let x = 0, y = 0, s = 10;
+let r1 = document.getElementById('im1');
+let r2 = document.getElementById('im2');
+let x1, y1;
 
 window.onload = function() {
-    const x1 = r1.offsetWidth;
-    const x2 = r2.offsetWidth;
-
-    function f() {
-        x += 10;
-        r2.style.left = x + 'px';
-
-        if (x < x1 - x2) {
-            setTimeout(f, 700);
-        }
-    }
-
+    x1 = r1.offsetWidth;
+    y1 = r1.offsetHeight;
     f();
 };
