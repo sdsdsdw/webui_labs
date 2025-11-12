379d34360bdff62335aa1b8ce123792c6c952758
sdsdsdw <148587087+sdsdsdw@users.noreply.github.com>
2025-11-12 12:58:36 +0000

    example 2

--- lab7/main.js
+++ lab7/main.js
@@ -1,16 +1,26 @@
-let i = 0;
+// let i = 0;
 
-function f() {
-    let r1 = document.getElementById('e1');
-    let r2 = document.getElementById('e2');
+// function f() {
+//     let r1 = document.getElementById('e1');
+//     let r2 = document.getElementById('e2');
 
-    if (i === 0) {
-        r1.style.color = 'green';
-        r2.value = 'змінити на червоний';
-        i = 1;
+//     if (i === 0) {
+//         r1.style.color = 'green';
+//         r2.value = 'змінити на червоний';
+//         i = 1;
+//     } else {
+//         r1.style.color = 'red';
+//         r2.value = 'змінити на зелений';
+//         i = 0;
+//     }
+// }
+
+function displ() {
+    let p = document.getElementById('st');
+    
+    if (p.style.display === 'none') {
+        p.style.display = 'block';
     } else {
-        r1.style.color = 'red';
-        r2.value = 'змінити на зелений';
-        i = 0;
+        p.style.display = 'none';
     }
 }
