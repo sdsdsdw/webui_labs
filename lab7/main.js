d0123a29763f211609aa0c4f4648524a7c0a7a57
sdsdsdw <148587087+sdsdsdw@users.noreply.github.com>
2025-11-12 09:56:10 +0000

    example 1

---
+++ lab7/main.js
@@ -0,0 +1,16 @@
+let i = 0;
+
+function f() {
+    let r1 = document.getElementById('e1');
+    let r2 = document.getElementById('e2');
+
+    if (i === 0) {
+        r1.style.color = 'green';
+        r2.value = 'змінити на червоний';
+        i = 1;
+    } else {
+        r1.style.color = 'red';
+        r2.value = 'змінити на зелений';
+        i = 0;
+    }
+}
